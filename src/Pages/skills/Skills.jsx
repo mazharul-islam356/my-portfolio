178c1059117d3c47e3be792e60317e9a50358383
rifat3568 <mazharulislam3569@gmail.com>
2024-01-09 11:27:16 +0600

    progress bar added

--- src/Pages/skills/Skills.jsx
+++ src/Pages/skills/Skills.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/no-unknown-property */
+import './skills.css'
+import './skills.js'
+
 
 const Skills = () => {
     return (
@@ -5,7 +9,27 @@ const Skills = () => {
 
 <h1 className="text-2xl font-semibold text-center">Skills</h1>
             <div className="grid text-center mt-4 grid-cols-5 justify-center items-center justify-items-center">
-            <p className="text-xl">Html</p>
+
+            {/* progress bar */}
+            <div className="skills">
+            <div className="outer ">
+                <div className="inner">
+            <div id="number">65%</div>
+                </div>
+            </div>
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+<defs>
+<linearGradient id="GradientColor">
+<stop offset="0%" stop-color="#DA22FF" />
+<stop offset="100%" stop-color="#9733EE"/>
+</linearGradient>
+</defs>
+<circle cx="80" cy="80" r="70" stroke-linecap="round" />
+</svg>
+            </div>
+
+
+
             <p className="text-xl">CSS</p>
             <p className="text-xl">Tailwind</p>
             <p className="text-xl">React</p>
