21fb45f55fe85d8df9296f0b7a39e5cf87bb821b
rifat3568 <mazharulislam3569@gmail.com>
2024-01-31 21:55:11 +0600

    banner image side cercle edited

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,53 @@
-
+import './home.css'
 const Home = () => {
     return (
         <div>
             
-            <div className="flex h-screen w-full justify-evenly space-y-3 gap-8 items-center">
+            <div className="flex h-screen w-full items-center justify-evenly space-y-3 gap-64 justify-items-center">
                 <div>
-                    <h3 className="text-3xl">Hello</h3>
-                    <h2 className="text-2xl">I am <span className="font-semibold">Mazharul Islam</span></h2>
-                    <h3 className="text-xl">Frontend developer</h3>
+                  
+                <h2 className="text-2xl w-96 mx-auto">I am <span className="font-semibold">Mazharul Islam</span></h2>
+                    <h3 className="text-xl w-96 mx-auto">Frontend developer</h3>
                     <p className="w-96 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, neque!Lorem</p>
-                    <button className="btn btn-outline mt-2">Download CV</button>
+                    <button className="btn btn-outline">Download CV</button>
+
                 </div>
-                <div>
-                    <img className="rounded-full w-80" src="https://i.ibb.co/FKgWFH2/mngr.jpg" alt="" />
-                </div>
+                
+                <section className="main-container">
+  <div className="main">
+    <div className="big-circle">
+      <div className="icon-block">
+        <img src="https://www.yudiz.com/codepen/animated-portfolio/web-dev-icon.png" alt="web design icon" />
+      </div>
+      <div className="icon-block">
+        <img src="https://www.yudiz.com/codepen/animated-portfolio/game-design-icon.png" alt="game design icon" />
+      </div>
+      <div className="icon-block">
+        <img src="https://www.yudiz.com/codepen/animated-portfolio/game-dev-icon.png" alt="game dev icon" />
+      </div>
+      <div className="icon-block">
+        <img src="https://www.yudiz.com/codepen/animated-portfolio/ui-ux-icon.png" alt="ui-ux icon" />
+      </div>
+    </div>
+    <div className="circle1">
+      <div className="icon-block">
+        <img src="https://www.yudiz.com/codepen/animated-portfolio/app-icon.png" alt="app icon" />
+      </div>
+      <div className="icon-block">
+        <img src="https://www.yudiz.com/codepen/animated-portfolio/blockchain-icon.png" alt="blockchain icon" />
+      </div>
+      <div className="icon-block">
+        <img src="https://www.yudiz.com/codepen/animated-portfolio/arvr-icon.png" alt="ar-vr icon" />
+      </div>
+      <div className="icon-block">
+        <img src="https://www.yudiz.com/codepen/animated-portfolio/artificial-intelligence-icon.png" alt="artificial intelligence icon" />
+      </div>
+    </div>
+    <div className="center-logo p-10">
+      <img className='rounded-full w-52 h-56 ' src="https://i.ibb.co/55vQG7W/mazhar.jpg" alt="logo" />
+    </div>
+  </div>
+</section>
             </div>
 
         </div>
