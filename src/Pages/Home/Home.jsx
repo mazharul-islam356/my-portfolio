dd1bb7827792eef52f3e52987f108c92182af11f
rifat3568 <mazharulislam3569@gmail.com>
2024-02-01 20:56:58 +0600

    animation edited

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import './home.css'
 const Home = () => {
     return (
-        <div>
+        <div className="bg-[url('https://i.ibb.co/ThFKHm4/gg.jpg')] bg-no-repeat">
             
             <div className="flex h-screen w-full items-center justify-evenly space-y-3 gap-64 justify-items-center">
                 <div>
@@ -17,30 +17,30 @@ const Home = () => {
   <div className="main">
     <div className="big-circle">
       <div className="icon-block">
-        <img src="https://www.yudiz.com/codepen/animated-portfolio/web-dev-icon.png" alt="web design icon" />
+        <img src="https://i.ibb.co/JjzqBQ0/html-5-1.png" alt="web design icon" />
       </div>
       <div className="icon-block">
-        <img src="https://www.yudiz.com/codepen/animated-portfolio/game-design-icon.png" alt="game design icon" />
+        <img src="https://i.ibb.co/JFgxHJW/css-3.png" alt="game design icon" />
       </div>
       <div className="icon-block">
-        <img src="https://www.yudiz.com/codepen/animated-portfolio/game-dev-icon.png" alt="game dev icon" />
+        <img src="https://i.ibb.co/djKctCZ/tailwind-css-256x154.png" alt="game dev icon" />
       </div>
       <div className="icon-block">
-        <img src="https://www.yudiz.com/codepen/animated-portfolio/ui-ux-icon.png" alt="ui-ux icon" />
+        <img src="https://i.ibb.co/CH2mfhW/javascript-js-512x512.png" alt="js" />
       </div>
     </div>
     <div className="circle1">
-      <div className="icon-block">
-        <img src="https://www.yudiz.com/codepen/animated-portfolio/app-icon.png" alt="app icon" />
+      <div className="icon-block-f">
+        <img src="https://i.ibb.co/sRLTwWW/file-type-firebase-187x256.png" alt="firebase" />
       </div>
       <div className="icon-block">
-        <img src="https://www.yudiz.com/codepen/animated-portfolio/blockchain-icon.png" alt="blockchain icon" />
+        <img src="https://i.ibb.co/DwdnMTd/node-228x256.png" alt="nodejs" />
       </div>
       <div className="icon-block">
-        <img src="https://www.yudiz.com/codepen/animated-portfolio/arvr-icon.png" alt="ar-vr icon" />
+        <img src="https://i.ibb.co/1Q0L0YR/mongodb-original-wordmark-505x512.png" alt="mongodb" />
       </div>
       <div className="icon-block">
-        <img src="https://www.yudiz.com/codepen/animated-portfolio/artificial-intelligence-icon.png" alt="artificial intelligence icon" />
+        <img src="https://i.ibb.co/Bg7T9Kv/file-type-reactjs-256x228.png" alt="react" />
       </div>
     </div>
     <div className="center-logo p-10">
