5dd623fe688411dca89450d6d51288219e211bcb
rifat3568 <mazharulislam3569@gmail.com>
2024-02-10 21:57:43 +0600

    added 1st project name in projects card

--- src/Pages/projects/Projects.jsx
+++ src/Pages/projects/Projects.jsx
@@ -30,8 +30,8 @@ const Projects = () => {
       <img src="https://i.ibb.co/H2FRS62/copy-writer.jpg" className="animate-fade-in block h-[20rem] w-full -ml-14 scale-100 transform object-cover object-center opacity-100 transition duration-300 group-hover:scale-110" alt="" />
     </div>
     <div className="absolute bottom-0 z-20 m-0 pb-4 ps-4 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110">
-      <h1 className="font-serif text-2xl font-bold text-white shadow-xl">Azores</h1>
-      <h1 className="text-sm font-light text-gray-200 shadow-xl">A Little Paradise in Portugal</h1>
+      <h1 className="font-serif text-3xl font-bold text-black shadow-xl">CareerFinder</h1>
+      <h1 className="text-xl text-black font-medium shadow-xl">A job seeking website</h1>
     </div>
   </div>
 </div>
