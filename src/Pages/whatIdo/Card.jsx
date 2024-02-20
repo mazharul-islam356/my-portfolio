be143288889baf030227d00cb46cfea006b4947f
rifat3568 <mazharulislam3569@gmail.com>
2024-02-20 21:56:51 +0600

    add last card name and img

--- src/Pages/whatIdo/Card.jsx
+++ src/Pages/whatIdo/Card.jsx
@@ -84,13 +84,13 @@ const Card = () => {
           <div data-aos-duration="800" data-aos="fade-left" className="cards rounded-2xl shadow-xl">
   
     <a href="" className="card">
-      <img src="https://i.imgur.com/oYiTqum.jpg" className="card__image" alt="" />
+      <img src="https://i.ibb.co/QM9qfqM/DM-blog-post-image-03.jpg" className="card__image" alt="" />
       <div className="card__overlay">
         <div className="card__header">
           <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
           
           <div className="card__header-text">
-            <h3 className="card__title">Jessica Parker</h3>            
+            <h3 className="card__title font-semibold">Digital Marketing</h3>            
             <span className="card__status">1 hour ago</span>
           </div>
         </div>
