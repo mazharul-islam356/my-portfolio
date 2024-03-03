3c1ea44076494912416657a7cfbc78d534531f90
rifat3568 <mazharulislam3569@gmail.com>
2024-03-03 23:03:06 +0600

    fun facts text edited

--- src/Pages/funFacts/FunFacts.jsx
+++ src/Pages/funFacts/FunFacts.jsx
@@ -19,8 +19,8 @@ const FunFacts = () => {
             <div className='grid grid-cols-3 mt-16 justify-center items-center '>
                 <div className='flex flex-col justify-center items-center gap-1'>
                 <TbMoodHappyFilled  className='text-2xl text-center'/>
-                    <h1 className='text-2xl'>Happy Clints</h1>
-                    <h1 className="text-3xl font-semibold  lg:leading-normal leading-7 text-[#101010]">
+                    <h1 className='lg:text-2xl'>Happy Clints</h1>
+                    <h1 className="lg:text-3xl font-semibold  lg:leading-normal leading-7 text-[#101010]">
                             <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
                                 {
                                     counterOn && <CountUp start={0} end={4} duration={2} delay={0} />
@@ -31,8 +31,8 @@ const FunFacts = () => {
 
                 <div className='flex flex-col justify-center items-center gap-1'>
                 <GoProjectRoadmap  className='text-2xl text-center font-bold'/>
-                    <h1 className='text-2xl'>Completed projects</h1>
-                    <h1 className="text-3xl font-semibold  lg:leading-normal leading-7 text-[#101010]">
+                    <h1 className='lg:text-2xl text-center'>Completed projects</h1>
+                    <h1 className="lg:text-3xl font-semibold  lg:leading-normal leading-7 text-[#101010]">
                             <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
                                 {
                                     counterOn && <CountUp start={0} end={20} duration={2} delay={0} />
@@ -43,8 +43,8 @@ const FunFacts = () => {
 
                 <div className='flex flex-col justify-center items-center gap-1'>
                 <IoHourglassOutline  className='text-2xl text-center'/>
-                    <h1 className='text-2xl'>Working hour</h1>
-                    <h1 className="text-3xl font-semibold  lg:leading-normal leading-7 text-[#101010]">
+                    <h1 className='lg:text-2xl'>Working hour</h1>
+                    <h1 className="lg:text-3xl font-semibold  lg:leading-normal leading-7 text-[#101010]">
                             <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
                                 {
                                     counterOn && <CountUp start={0} end={165} duration={2} delay={0} />
