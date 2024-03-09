9e961ce6e262264725c91dda4da578cbfa2233e2
rifat3568 <mazharulislam3569@gmail.com>
2024-03-09 19:58:29 +0600

    photo galary add

--- src/Pages/Photography/Photography.jsx
+++ src/Pages/Photography/Photography.jsx
@@ -1,9 +1,44 @@
+import './photography.css'
+
 
 const Photography = () => {
     return (
         <div>
             <div>
             <h1 className="text-2xl font-semibold text-center">Photography</h1>
+            
+
+            <div className='flex justify-center my-40'>
+            <div className="scope">
+<span style={{'--i': 1}}>
+  <img src="https://i.ibb.co/QM9qfqM/DM-blog-post-image-03.jpg" alt="not found" />
+</span>
+<span style={{'--i': 2}}>
+  <img src="https://i.ibb.co/QM9qfqM/DM-blog-post-image-03.jpg" alt="not found" />
+</span>
+<span style={{'--i': 3}}>
+  <img src="https://i.ibb.co/QM9qfqM/DM-blog-post-image-03.jpg" alt="not found" />
+</span>
+<span style={{'--i': 4}}>
+  <img src="https://i.ibb.co/QM9qfqM/DM-blog-post-image-03.jpg" alt="not found" />
+</span>
+<span style={{'--i': 5}}>
+  <img src="https://i.ibb.co/QM9qfqM/DM-blog-post-image-03.jpg" alt="not found" />
+</span>
+<span style={{'--i': 6}}>
+  <img src="https://i.ibb.co/QM9qfqM/DM-blog-post-image-03.jpg" alt="not found" />
+</span>
+<span style={{'--i': 7}}>
+  <img src="https://i.ibb.co/QM9qfqM/DM-blog-post-image-03.jpg" alt="not found" />
+</span>
+<span style={{'--i': 8}}>
+  <img src="https://i.ibb.co/QM9qfqM/DM-blog-post-image-03.jpg" alt="not found" />
+</span>
+
+  
+                </div>
+            </div>
+
             </div>
         </div>
     );
